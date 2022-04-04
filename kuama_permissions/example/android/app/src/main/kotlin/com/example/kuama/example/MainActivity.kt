f65501e98a2c1314a3a0ec5b236ab19362a4b823
BreX900 <brexmaster@gmail.com>
2022-04-04 10:39:51 +0200

    feat add example app feat convert AppLifecycleStateRepository to singleton

---
+++ kuama_permissions/example/android/app/src/main/kotlin/com/example/kuama/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kuama.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- kuama_permissions/lib/kuama_permissions.dart
+++ kuama_permissions/lib/kuama_permissions.dart
@@ -1,9 +1,12 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-library lib;
+library kuama_permissions;
+
+export 'package:permission_handler/permission_handler.dart' show Permission, PermissionStatus;
 
 export 'src/blocs/permissions_bloc.b.dart';
 export 'src/failures.dart';
+export 'src/repositories/app_lifecycle_state_repository.dart';
 export 'src/repositories/permissions_manger_repository.dart';
 export 'src/repositories/permissions_preferences_repository.dart';
 export 'src/services/permissions_service.dart';
